522bf1242331a0247616e9978562ccc6891e18c2
Lito Croy <146680987+litobot@users.noreply.github.com>
2024-10-02 16:16:05 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Ideas from './Ideas';
+import { useState } from 'react'
+import Form from './Form';
 
-function App() {
+function App (){
+  const dummyIdeas = [
+    { id: 1, title: 'Prank Travis', description: 'Stick googly eyes on all his stuff' },
+    { id: 2, title: 'Make a secret password app', description: 'So you and your rideshare driver can both know neither one of you is lying' },
+    { id: 3, title: 'Learn a martial art', description: 'To exact vengeance upon my enemies' },
+]
+  const [ideas, setIdeas] = useState(dummyIdeas)
+
+  function addIdea(newIdea){
+    setIdeas([...ideas, newIdea]);
+  }
+
+  function deleteIdea(id){
+    console.log(id);
+    const filteredIdeas = ideas.filter(idea => idea.id !== id)
+    setIdeas(filteredIdeas)
+    }
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main className='App'>
+      <h1>IdeaBox</h1>
+      <Form addIdea={addIdea} />  {/* passing fx `addIdea` as props */}
+      <Ideas ideas={ideas} deleteIdea={deleteIdea} /> {/* passing `ideas` as props */}
+    </main>
+  )
 }
 
 export default App;
---
+++ src/Card.js
@@ -0,0 +1,12 @@
+import './Card.css';
+
+const Card = ({ title, description, id, deleteIdea }) => {
+    return (
+      <div className='card'>
+        <h3>{title}</h3>
+        <p>{description}</p>
+        <button onClick={()=> deleteIdea(id)}>🗑</button>
+      </div>
+    )
+  }
+  export default Card;
